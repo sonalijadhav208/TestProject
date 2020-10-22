24e910f4458af9fbd35ebf4c82b99d830655b273
sonali <sonalijadhav208@gmail.com>
2020-10-22 11:17:53 +0530

    Webinar_GitHub - Code review input fixes Iteration 1

--- TestGitProject/src/pracProject/Test.java
+++ TestGitProject/src/pracProject/Test.java
@@ -2,7 +2,9 @@ package pracProject;
 
 public class Test {
 	
-	// I am testing Github_Webinar branch
+	/*
+	 *I am testing Github_Webinar branch
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
